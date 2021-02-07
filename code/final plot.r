6f4dafdca0a6e0e6a590c3cb261bc9565116463f
Christian Devenish <Cdevenish@users.noreply.github.com>
2021-02-07 19:03:49 +0000

    set up repo and load code

---
+++ code/final plot.r
@@ -0,0 +1,35 @@
+####### Final plots
+
+source("https://raw.githubusercontent.com/Cdevenish/R-Material/master/Functions/w.xls.r")
+
+load("sightings.rdata")
+years
+
+dataX <- cbind(years = years, PXt)
+
+load("specimen.rdata")
+
+dataX <- data.frame(rbind(dataX, cbind(years = years, PXt)))
+colnames(dataX) <- c("years", "sd_lwr", "PXt", "sd_upr", "PXt_min", "PXt_max")
+
+dataX$Basis <- rep(c("All records", "Specimens"), each = length(years))
+head(dataX)
+
+w.xls(dataX)
+
+library(ggplot2)
+
+ggplot(dataX, aes(x = years, y = PXt, col= Basis))+
+  geom_ribbon(aes(ymin=sd_lwr, ymax = sd_upr, fill = Basis), linetype = 0, alpha = 0.4)+
+  geom_ribbon(aes(ymin=PXt_min, ymax = PXt_max, fill = Basis), linetype = 0, alpha = 0.2)+
+  scale_color_manual(values = c("black", "darkred"))+
+  scale_fill_manual(aesthetics = "fill", values = c("grey10", "grey50"))+
+  geom_line(aes(linetype= Basis), size = 1)+
+  ylim(c(0,1))+
+  xlab("Years")+
+  scale_x_continuous(breaks = seq(1890, 2030,10))+
+  ylab("Probability that taxon is extant")+
+  theme_light()+
+  theme(legend.position = c(0.1,0.15))
+  ggsave("extinction risk/risk_2_on_1.png", width = 200, height = 150, units = "mm")
+
